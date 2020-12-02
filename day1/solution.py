6fc494c6916ee3ae311be0331621ba35e5f6b552
Jamey Gronewald <jrgronewald@gmail.com>
2020-12-01 21:47:13 -0800

    Day 1 second solution complete in python

--- day1/solution.py
+++ day1/solution.py
@@ -8,15 +8,16 @@ def create_list_from_expense_report():
     return expense_list
 
 def find_product(expense_list):
-    expenses_to_multiply = []
     for x in expense_list:
-        try:
-            index_to_multiply = expense_list.index(2020 - x)
-            expenses_to_multiply.append(expense_list[index_to_multiply])
-            if len(expenses_to_multiply) == 2:
-                return print(expenses_to_multiply[0] * expenses_to_multiply[1])
-        except ValueError:
-            continue
+        for y in expense_list:
+            if x == y:
+                continue
+            try:
+                index_to_multiply = expense_list.index(2020 - x - y)
+                expenses_to_multiply = [x, y, expense_list[index_to_multiply]]
+                return print(expenses_to_multiply[0] * expenses_to_multiply[1] * expenses_to_multiply[2])
+            except ValueError:
+                continue
 
 if __name__ == "__main__":              
     find_product(create_list_from_expense_report())
