d0129268dcc2478621c54821bf40673f42e692ce
Jamey Gronewald <jrgronewald@gmail.com>
2020-12-10 09:26:10 -0800

    day10 solutionA complete

---
+++ day10/solutionA.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('input.txt', 'utf-8').split('\n');
+
+const sorted = data.sort((a, b) => a - b);
+
+let ones = 1;
+let threes = 1;
+
+for (let i = 0; i < sorted.length - 1; i++) {
+  if (sorted[i + 1] - sorted[i] === 1) ones++;
+  else threes++;
+}
+
+console.log(ones, threes, ones * threes);
